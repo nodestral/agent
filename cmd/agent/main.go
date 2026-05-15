1411acea6482ce38b7c67a81c739c572245927c7
RhythmWave <kleirose.bayu@gmail.com>
2026-05-15 16:28:04 +0800

    release v0.2.2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -17,7 +17,7 @@ import (
   "github.com/nodestral/agent/pkg/system"
 )
 
-const version = "0.2.1"
+const version = "0.2.2"
 
 func main() {
   configPath := flag.String("config", "", "path to agent config file")
